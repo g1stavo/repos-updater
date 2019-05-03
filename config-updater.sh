640226b9c938f7980ec7e3921dce6c91c9f43544
Gustavo <gustavocsalvador@gmail.com>
2019-05-03 19:18:58 -0300

    Update config-updater.sh

--- config-updater.sh
+++ config-updater.sh
@@ -1,11 +1,13 @@
 #!bin/bash
 APIKEY="$1"
 BRANCH_NAME="update-ci-config"
+COMMIT_MESSAGE="Update .circleci/config.yml"
+
 git clone git@github.com:chaordic/theme-"${APIKEY}"
 cd theme-"${APIKEY}"
 git checkout -b "${BRANCH_NAME}"
 rm .circleci/config.yml
 cp ../config.yml .circleci/
 git add .
-git commit -m "Update .circleci/config.yml"
-git push origin update-ci-config
+git commit -m "${COMMIT_MESSAGE}"
+git push origin "${BRANCH_NAME}"
