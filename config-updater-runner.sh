e02f65b1a96288395e9ca48a5cb7465a5148b6af
Gustavo <gustavocsalvador@gmail.com>
2019-05-06 19:31:45 -0300

    Centralize

--- config-updater-runner.sh
+++ config-updater-runner.sh
@@ -1,3 +1,14 @@
-#!bin/bash
+#!/bin/bash
 APIKEYS=("beautybox-vtex" "cea" "centauro-v5" "chillibeans" "compracerta" "consul" "cursoslivresead" "deliverysupermuffato" "drogariaaraujo" "dutramaquinas" "electrolux" "gaston" "grupo-portaleducacao" "imaginarium-vtex" "madeiramadeira" "malwee" "marisa" "paqueta" "paquetaesportes" "saraiva-v5" "savegnago" "sportline" "velez") 
-for i in "${APIKEYS[@]}"; do ./config-updater.sh $i; done
+STATUS=''
+HAS_CONFIG=()
+
+existsConfig() {
+    STATUS=$(curl -s -o /dev/null -w "%{http_code}" https://api.github.com/repos/chaordic/theme-"$i"/contents/.circleci/config.yml -u g1stavo:'J*r-Q=4Zx<mcfL3G}e^]zV(h,D^mmBun99raK+Q>x.$<~wVBj5j4D;->fjW%C"\\tR:A9Fw&')
+    if [[ $STATUS -eq "200" ]] ; then
+        HAS_CONFIG+="$i"
+    fi
+}
+
+for i in "${APIKEYS[@]}"; do existsConfig ; done
+for j in "${HAS_CONFIG[@]}"; do ./config-updater.sh $j ; done
