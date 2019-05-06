4065ddcc3443ee80ee517e9fc3b0cad488a75256
Gustavo <gustavocsalvador@gmail.com>
2019-05-06 19:23:53 -0300

    Add hasConfig script

--- config-updater-runner.sh
+++ config-updater-runner.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-APIKEYS=("saraiva-v5" "centauro-v5" "cea" "malwee" "marisa" "paquetaesportes")
+APIKEYS=("beautybox-vtex" "cea" "centauro-v5" "chillibeans" "compracerta" "consul" "cursoslivresead" "deliverysupermuffato" "drogariaaraujo" "dutramaquinas" "electrolux" "gaston" "grupo-portaleducacao" "imaginarium-vtex" "madeiramadeira" "malwee" "marisa" "paqueta" "paquetaesportes" "saraiva-v5" "savegnago" "sportline" "velez") 
 for i in "${APIKEYS[@]}"; do ./config-updater.sh $i; done
