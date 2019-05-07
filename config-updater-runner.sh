e76f61d2f7001f460df7f8aa728b35d3a07e4dd1
Gustavo <gustavocsalvador@gmail.com>
2019-05-07 14:18:38 -0300

    Add tools

--- config-updater-runner.sh
+++ config-updater-runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-APIKEYS=("beautybox-vtex" "cea" "centauro-v5" "chillibeans" "compracerta" "consul" "cursoslivresead" "deliverysupermuffato" "drogariaaraujo" "dutramaquinas" "electrolux" "gaston" "grupo-portaleducacao" "imaginarium-vtex" "madeiramadeira" "malwee" "marisa" "paqueta" "paquetaesportes" "saraiva-v5" "savegnago" "sportline" "velez") 
+APIKEYS=("tools" "beautybox-vtex" "cea" "centauro-v5" "chillibeans" "compracerta" "consul" "cursoslivresead" "deliverysupermuffato" "drogariaaraujo" "dutramaquinas" "electrolux" "gaston" "grupo-portaleducacao" "imaginarium-vtex" "madeiramadeira" "malwee" "marisa" "paqueta" "paquetaesportes" "saraiva-v5" "savegnago" "sportline" "velez") 
 STATUS=''
 HAS_CONFIG=()
 
@@ -7,6 +7,7 @@ existsConfig() {
     STATUS=$(curl -s -o /dev/null -w "%{http_code}" https://api.github.com/repos/chaordic/theme-"$i"/contents/.circleci/config.yml -u g1stavo:'J*r-Q=4Zx<mcfL3G}e^]zV(h,D^mmBun99raK+Q>x.$<~wVBj5j4D;->fjW%C"\\tR:A9Fw&')
     if [[ $STATUS -eq "200" ]] ; then
         HAS_CONFIG+=("$i")
+        echo "$i" 
     fi
 }
 
